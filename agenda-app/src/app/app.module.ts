5f3c138d4d98b5f4dd179f065f5e011f901fed5f
tarcio almeida <tarcioalmeidalima@gmail.com>
2020-12-08 00:26:05 -0300

    Criando componente e serviço de contatos

--- agenda-app/src/app/app.module.ts
+++ agenda-app/src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ContatoComponent } from './contato/contato.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContatoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
